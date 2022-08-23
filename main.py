54ef3d9a3bf2f0c3f1dee3417297f16414e71395
jchen65 <50344357+jchen65@users.noreply.github.com>
2022-08-23 11:07:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -24,15 +24,6 @@ def get_weather():
   weather = res['data']['list'][0]
   return weather['weather'], math.floor(weather['temp']), math.floor(weather['high']), math.floor(weather['low'])
 
-def get_count():
-  delta = today - datetime.strptime(start_date, "%Y-%m-%d")
-  return delta.days
-
-def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
