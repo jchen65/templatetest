cb2e453b5fb5045e354208d00cabad7226c600ac
jchen65 <50344357+jchen65@users.noreply.github.com>
2022-08-23 11:15:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,10 +39,8 @@ client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
 wea, temperature, highest, lowest = get_weather()
-data = {"weather":{"value":wea,"color":get_random_color()},"temperature":{"value":temperature,"color":get_random_color()},"love_days":{"value":get_count(),"color":get_random_color()},"birthday_left":{"value":get_birthday(),"color":get_random_color()},"words":{"value":get_words(),"color":get_random_color()},"highest": {"value":highest,"color":get_random_color()},"lowest":{"value":lowest, "color":get_random_color()}}
-count = 0
+data = {"weather":{"value":wea,"color":get_random_color()},"temperature":{"value":temperature,"color":get_random_color()},"words":{"value":get_words(),"color":get_random_color()},"highest": {"value":highest,"color":get_random_color()},"lowest":{"value":lowest, "color":get_random_color()}}
 for user_id in user_ids:
   res = wm.send_template(user_id, template_id, data)
-  count+=1
 
-print("发送了" + str(count) + "条消息")
+print("发送了消息")
